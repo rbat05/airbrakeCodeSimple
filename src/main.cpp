94a703e87648c6cbe66fee6533d89bcec03fdfe2
G3ORGE-S <gsen147@aucklanduni.ac.nz>
2026-05-07 16:38:31 +1200

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,29 @@
 #include "hexdump.h"
 #include "imu.h"
 
+// Controller
+#include "Dynamics.h"
+#include "RocketVariables.h"
+
 #define SAMPLE_RATE_MS 100  // 10 Hz
+
+
+  // MODE
+  bool fakeFlightTest = true;  // SET TO 1 WHEN LAUNCHING ACTUAL ROCKET , 0 otherwise
+  bool realFlight = false;
+  // Only 1 of these modes can be set to value of 1.
+
+  
+// control
+float u = 0.0f;
+float u_prev = 0.0f;
+
+
+
+// params
+// predictapogee dt = 0.02f; 
+// main loop dt - IMPORTANT - run tests to determine this value.
+
 
 void setup() {
   Serial.begin(115200);
@@ -30,6 +52,20 @@ void setup() {
 
   Serial.printf("[MAIN] Logging binary sensor data at %d Hz\n\n",
                 1000 / SAMPLE_RATE_MS);
+
+
+
+
+  if (fakeFlightTest == realFlight) {
+    realFlight = true;
+    fakeFlightTest = false;
+  }
+
+  if (fakeFlightTest == 1) {
+    state.h = 120.0;
+    state.v = 105.3;
+  }
+             
 }
 
 void loop() {
@@ -38,4 +74,22 @@ void loop() {
 
   logSensorsBin(imu, baro);
   delay(SAMPLE_RATE_MS);
+
+
+
+  // Run fake test
+  if (fakeFlightTest == 1) {
+
+   
+      u = OptimiseControlInputBinarySearchConstraint(state.h, state.v,u_prev,0);
+
+      float h_pred = PredictApogee(state.h, state.v, u);
+
+      // ---- actuator ----
+      // set_servo(u);
+
+
+
+  }
+
 }
